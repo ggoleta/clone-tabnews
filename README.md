d441681ff73392cea6806d1272e5b6586527f8b1
Rogério Fernandes <rogerio_contato@hotmail.com>
2024-09-02 16:50:20 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 A project to improve my skills as a Full Stack Developer
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Roberta Cristina Barbosa, me comprometo a fazer você a mulher mais feliz do Mundo!!!</h1>
+  return (
+    <h1>
+      Roberta Cristina Barbosa, me comprometo a fazer você a mulher mais feliz
+      do Mundo!!!
+    </h1>
+  );
 }
 
 export default Home;
